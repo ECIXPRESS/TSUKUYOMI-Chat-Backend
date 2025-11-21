3c32b05de26fd94a0aecd1ce002c4a8d8fe5c208
nikolas.martinezrive <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-11-21 18:55:08 -0500

    fix: Modificacion cluster

--- src/main/java/edu/dosw/infrastructure/configs/WebSocketConfig.java
+++ src/main/java/edu/dosw/infrastructure/configs/WebSocketConfig.java
@@ -6,20 +6,26 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic"); //servidor a cliente
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // servidor hacia el cliente
+        config.enableSimpleBroker("/topic", "/queue");
 
+        // cliente hacia el servidor
+        config.setApplicationDestinationPrefixes("/app");
+
+        // Para mensajes privados a usuarios específicos
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
-
 }
